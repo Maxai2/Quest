1a33e43759ba70559fe379004206f285d3ea9bb0
Хамелеон <maxali946@gmail.com>
2017-10-24 17:58:25 +0400

    8%

--- Quest/Quest/Source.cpp
+++ Quest/Quest/Source.cpp
@@ -1,55 +1,55 @@
 #include <SFML/Graphics.hpp>
-#include <iostream>
-#include <string>
-#include <vector>
-
-using namespace std;
-//---------------------------------------------------------------------------------------
+//#include <iostream>
+//#include <string>
+//#include <vector>
 //
-///*
-//Files
-//const, final, override
-//smart pointers
-//*/
+//using namespace std;
+////---------------------------------------------------------------------------------------
+////
+/////*
+////Files
+////const, final, override
+////smart pointers
+////*/
+////
 //
-
-//---------------------------------------------------------------------------------------
-void main()
-{
-	//Game initialization
-
-	//Game loop
-	while (true)
-	{
-		//Input handling
-
-		//Game logic
-
-		//Drawing
-	}
-}
-//---------------------------------------------------------------------------------------
-
-
-//int main()
+////---------------------------------------------------------------------------------------
+//void main()
 //{
-//	sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-//	sf::CircleShape shape(100.f);
-//	shape.setFillColor(sf::Color::Green);
+//	//Game initialization
 //
-//	while (window.isOpen())
+//	//Game loop
+//	while (true)
 //	{
-//		sf::Event event;
-//		while (window.pollEvent(event))
-//		{
-//			if (event.type == sf::Event::Closed)
-//				window.close();
-//		}
+//		//Input handling
 //
-//		window.clear();
-//		window.draw(shape);
-//		window.display();
+//		//Game logic
+//
+//		//Drawing
 //	}
-//
-//	return 0;
 //}
+////---------------------------------------------------------------------------------------
+
+int main()
+{
+	sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+	sf::CircleShape shape(100.f);
+	shape.setFillColor(sf::Color::Green);
+
+	while (window.isOpen())
+	{
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
+
+		window.clear();
+		window.draw(shape);
+		window.display();
+	}
+
+	return 0;
+}
+
